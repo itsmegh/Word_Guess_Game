04c4c8b9566c2fcec6d470af3bea70fda92f09cc
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-02 14:57:11 -0600

    Javascript code for game to test

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,12 +17,32 @@ var remainingLetters = song.length;
 
 //Game Loop
 
-//use report but then need to get the answer on the page--text.content?
 //show the player's guesses
 while (remainingLetters > 0) {
     report(answerArray.join(""));
 }
 
 //Player input
-var guess = document.getElementById("guessTextArea")
+var guess = document.getElementById("exampleFormControlTextarea1")
+
+document.onkeyup = function(event) {
+    guess.textContent = event.key;
+}
+//Get a guess from the player
+if (guess.length !== 1) {
+    alert("Chill! Enter one letter at a time.");
+} else {
+//Update the game with the guess
+    for (var j=0; j<song.length; j++) {
+        if (song[j] === guess) {
+            answerArray[j] = guess;
+            remainingLetters--;
+        }
+    }
+}
+//End of the game loop
+
+//Show the answer and win or loss response
+alert(answerArray.join(" "));
+alert("Far out! The answer was " + song);
 //var name = beatleSongList.toUpperCase to get all the letters to be capital during the word guess
