b9a7830ff7b9b4c1b446829c08fbc6ca8c51cf5e
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-02 15:21:44 -0600

    Working to link javascript to html page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,30 +19,30 @@ var remainingLetters = song.length;
 
 //show the player's guesses
 while (remainingLetters > 0) {
-    report(answerArray.join(""));
+    console.log(answerArray.join(" "));
 }
 
 //Player input
-var guess = document.getElementById("exampleFormControlTextarea1")
+var guess = document.getElementById("user-text")
 
 document.onkeyup = function(event) {
-    guess.textContent = event.key;
+     guess.textContent = event.key;
 }
-//Get a guess from the player
-if (guess.length !== 1) {
-    alert("Chill! Enter one letter at a time.");
-} else {
-//Update the game with the guess
-    for (var j=0; j<song.length; j++) {
-        if (song[j] === guess) {
-            answerArray[j] = guess;
-            remainingLetters--;
-        }
-    }
-}
-//End of the game loop
+// //Get a guess from the player
+// if (guess.length !== 1) {
+//     alert("Chill! Enter one letter at a time.");
+// } else {
+// //Update the game with the guess
+//     for (var j=0; j<song.length; j++) {
+//         if (song[j] === guess) {
+//             answerArray[j] = guess;
+//             remainingLetters--;
+//         }
+//     }
+// }
+// //End of the game loop
 
-//Show the answer and win or loss response
-alert(answerArray.join(" "));
-alert("Far out! The answer was " + song);
+// //Show the answer and win or loss response
+// alert(answerArray.join(" "));
+// alert("Far out! The answer was " + song);
 //var name = beatleSongList.toUpperCase to get all the letters to be capital during the word guess
