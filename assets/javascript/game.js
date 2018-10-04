a643b2267aa90e7ff63888c293e0fb79c7a3de00
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-04 14:01:12 -0600

    Commented out javascript and bootstrap design to rework the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,161 @@
-//Choosing a random word from the word bank
-var beatleSongList = [
-    "Hey Jude", "Here Comes the Sun", "Penny Lane", "I Am the Walrus", "While My Guitar Gently Weeps",
+// var $ = function (id) {
+//     return document.getElementById(id);
+// }
+
+var beatleWordList = [
+"hey jude", "walrus", "hello goodbye", "lennon", "revolution", "yellow submarine", 
+"blackbird", "beatles", "abbey road", "yesterday", "paperback writer", "starr", "mccartney", "liverpool", 
+"sgt pepper", "white album", "prudence", "rocky raccoon", "honey pie", "good night", "b side", 
+"vinyl", "helter skelter", "harrison", "julia", "warm gun", "glass onion", "penny lane", "strawberry fields", 
+"maggie mae", "kaleidoscope eyes", "marmalade skies", "tangerine trees", "nowhere man", "groovy",
 ]
 
-var song = beatleSongList[Math.floor(Math.random() * beatleSongList.length)];
+// var chosenWord = "";
+// var letterInChosenWord = [];
+// var numBlanks = 0;
+// var blanksAndCorrectLetters = [];
+// var wrongGuesses = [];
 
-//Creating the answer array to fill with _ that match the number of letters in a word
-// How to account for spaces???
+// var winCounter = 0;
+// var lossCounter = 0;
+// var numGuesses = 10;
 
-var answerArray = [];
-for(var i=0; i<song.length; i++) {
-    answerArray[i] = "_";
-}
+// function startGame() {
+//     wrongGuesses = [];
+//     console.log("this is a wrong guess in startGame", wrongGuesses);
+//     numGuesses = 10;
+//     blanksAndCorrectLetters = [];
 
-var remainingLetters = song.length;
+//     chosenWord = beatleWordList[Math.floor(Math.random() * beatleWordList.length)];
+//     letterInChosenWord = chosenWord.split("");
+//     numBlanks = letterInChosenWord.length;
+//     console.log(chosenWord);
+//     console.log(numBlanks);
 
-//Game Loop
+//     for(var i=0; i<numBlanks; i++) {
+//         blanksAndCorrectLetters.push("_");
+//     }
 
-//show the player's guesses
-while (remainingLetters > 0) {
-    console.log(answerArray.join(" "));
-}
+//     console.log(blanksAndCorrectLetters);
+//     console.log("Hello line 40")
+//     document.getElementById('word-blank').innerHTML = this.blanksAndCorrectLetters;
+//     document.getElementById('guesses-left').innerHTML = numGuesses;
 
-//Player input
-var guess = document.getElementById("user-text")
+// }
 
-document.onkeyup = function(event) {
-     guess.textContent = event.key;
-}
-// //Get a guess from the player
-// if (guess.length !== 1) {
-//     alert("Chill! Enter one letter at a time.");
-// } else {
-// //Update the game with the guess
-//     for (var j=0; j<song.length; j++) {
-//         if (song[j] === guess) {
-//             answerArray[j] = guess;
-//             remainingLetters--;
-//         }
+// function checkLetters(letter) {
+// //Compare the letter the player picks to the secret word, need a conditional
+// //statement to determine if the player picks a letter in the word. If not, do
+// //something else
+
+// var letterInWord = false;
+
+// for(var i=0; i<numBlanks; i++) {
+//     if(chosenWord[i] === letter) {
+//         letterInWord = true;
 //     }
 // }
-// //End of the game loop
 
-// //Show the answer and win or loss response
-// alert(answerArray.join(" "));
-// alert("Far out! The answer was " + song);
-//var name = beatleSongList.toUpperCase to get all the letters to be capital during the word guess
+// if(letterInWord) {
+//     for(i=0; i<numBlanks; i++) {
+//         if(chosenWord[i] === letter) {
+//             blanksAndCorrectLetters[i] = letter;
+//         }
+//     }
+// } else {
+//     numGuesses--;
+//     wrongGuesses.push(letter)
+// }
+
+//  //check to see if letter is already in the letters guess array.   
+
+// }
+// //Update HTML with letters that are in secret word, guesses remaining, show letters guessed, show if game was won or not
+// function roundComplete() {
+//     document.getElementById("word-blank").innerHTML = blanksAndCorrectLetters.join(" ");
+//     document.getElementById("guesses-left").innerHTML = numGuesses;
+//     document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+//     console.log(letterInChosenWord);
+//     console.log(blanksAndCorrectLetters);
+
+//     if(letterInChosenWord.join(" ") === blanksAndCorrectLetters.join(" ")) {
+//         winCounter++;
+//         alert("Far Out! You win!");
+//         document.getElementById("win-counter").innerHTML = winCounter;
+//         startGame();
+
+//     } else if(numGuesses === 0) {
+//         document.getElementById("loss-counter").innerHTML = lossCounter++;
+//         document.getElementById("wrong-guesses").innerHTML = "";
+//         alert("Bummer.... You lost.");
+//         startGame();
+//     }
+
+
+// }
+// startGame();
+// document.onkeyup = function(event) {
+//     var letterGuessed = String.fromCharCode(event.keyCode).toUpperCase();
+//     console.log("this is the letter we typed", letterGuessed);
+//     checkLetters(letterGuessed);
+//     roundComplete();
+// }
+
+// //to set only a-z pressed --if(event.keyCode >= 65 && event.keyCode <= 90) {
+
+
+// //document.onkeydown = function(event) {
+//     // If we finished a game, dump one keystroke and reset.
+  
+// // var choice = Math.floor(Math.random()*beatleWordList.length);
+// // var answer = beatleWordList[choice];
+// // var myLength = answer.length;
+// // var display = [myLength];
+// // var win = myLength;
+// // var letters = answer.split('');
+// // var attemptsLeft = 10;
+// // var output = "";
+// // var userLetter = "";
+
+// // var setup = function() {
+// //     for (var i=0; i<answer.length; i++)
+// //     {
+// //         display[i] = "_ ";
+// //         output = output + display[i];
+// //     }
+// //     document.getElementById("game").innerHTML=output;
+// //     output = "";
+// // }
+
+// // var submit = function()
+// // {
+// //     output = "";
+// //     userLetter=$("letter").value;
+// //     $("letter").value = "";
+
+// //     for (var c=0; c<answer.length; c++) {
+// //         return(letters[c]);
+// //         if(userLetter.toUpperCase() == letters[c])
+// //         {
+// //             display[c] = userLetter.toUpperCase();
+// //             win--;
+// //         }
+// //             output = output + display[c] + " ";
+// //     }
+// //     document.getElementById("game").innerHTML=output;
+// //     output="";
+// //     attemptsLeft--;
+
+// //     if (win < 1) {
+// //         document.getElementById("guesses").innerHTML = alert("Far out! You Win!");
+// //     } else if (attemptsLeft < 1) {
+// //         document.getElementById("guesses").innerHTML = alert("Bummer... You lose.");
+// //     } else {
+// //         document.getElementById("guesses").innerHTML = "<p>You have " + attemptsLeft + " guesses left</p>";
+// //     }
+// // }
+
+// // // window.onload = function() {
+// // //     setup();
+// // //     $("submit").onclick = submit;}
